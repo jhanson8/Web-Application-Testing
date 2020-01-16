5105416c38cf2655047f3d283530e90c2044bdf6
Jonathan Hanson <jhanson8@uoregon.edu>
2020-01-16 15:41:04 -0800

    functional app mvp

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Display from './components/display.js';
+import Dashboard from './components/dashboard.js';
+
 import './App.css';
 
 function App() {
+  const [strikes, setStrikes] = useState(0);
+  const [balls, setBalls] = useState(0);
+  const [hits, setHits] = useState(0);
+
+  const addStrike = event => {
+    setStrikes(strikes + 1);
+    if(strikes > 2){
+      setStrikes(0);
+    }
+
+  };
+
+  const addBall = event => {
+    setBalls(balls + 1);
+    if (balls >= 4){
+      setBalls(0);
+    }
+  };
+
+  const addFoul = event => {
+    setStrikes(strikes + 1);
+    if(strikes >= 2){
+      setStrikes(2);
+    }
+
+  };
+
+  const addHit = event => {
+
+      setHits(hits + 1);
+
+  }
+
+  const clearAll = () => {
+    return (
+      setStrikes(0),
+      setBalls(0),
+      setHits(0)
+    )
+  };
+
+  const resetCount = () => {
+    return (
+      setStrikes(0),
+      setBalls(0)
+
+    )
+  };
+
+
+    const resetHits = () => {
+      return (
+        setHits(0)
+
+      )
+    };
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<Display
+strikes={strikes}
+balls={balls}
+hits={hits}/>
+<Dashboard
+addStrike={addStrike}
+addBall={addBall}
+addFoul={addFoul}
+addHit={addHit}
+clearAll={clearAll}
+resetCount={resetCount}
+resetHits={resetHits}
+/>
     </div>
   );
 }
