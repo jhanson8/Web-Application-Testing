410443bf37a3cc1a855283415e112c0852fa9c5d
Jonathan Hanson <jhanson8@uoregon.edu>
2020-01-16 16:13:07 -0800

    first test

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App, {addStrike, addBall} from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+
+test('addStrike adds 1 strike to current strike count', () => {
+  let actual;
+  let expected;
+  actual = addStrike(1);
+  expected = 2;
+  expect(actual).toBe(expected);
+  expect(actual).not.toBe(3);
+
+  // litte cleaner tests
+  expect(addStrike(1)).toBe(2);
+  expect(addStrike(1)).not.toBe(3);
 });
